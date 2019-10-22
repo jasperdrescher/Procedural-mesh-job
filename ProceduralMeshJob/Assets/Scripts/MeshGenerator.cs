fc5c99624131fc232dae688624736844784c99c4
JasperDre <jasperdre@gmail.com>
2019-10-22 19:14:50 +0200

    Add procedural mesh generation

---
+++ ProceduralMeshJob/Assets/Scripts/MeshGenerator.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshGenerator : MonoBehaviour
+{
+    [SerializeField] private int myXLength;
+    [SerializeField] private int myZLength;
+
+    private Mesh myMesh;
+    private Vector3[] myVertices;
+    private int[] myTriangles;
+    private MeshFilter myMeshFilter;
+    private MeshCollider myMeshCollider;
+
+    private void Awake()
+    {
+        myMeshFilter = GetComponent<MeshFilter>();
+        myMeshCollider = GetComponent<MeshCollider>();
+
+        myMesh = new Mesh();
+        myMeshFilter.mesh = myMesh;
+        myMeshCollider.sharedMesh = myMesh;
+    }
+
+    private void Start()
+    {
+        GenerateMesh();
+        SetMesh();
+    }
+
+    private void GenerateMesh()
+    {
+        myVertices = new Vector3[(myXLength + 1) * (myZLength + 1)];
+
+        for (int zLength = 0, index = 0; zLength <= myZLength; zLength++)
+        {
+            for (int xLength = 0; xLength <= myZLength; xLength++)
+            {
+                myVertices[index] = new Vector3(xLength, 0, zLength);
+                index++;
+            }
+        }
+
+        int vertex = 0;
+        int triangles = 0;
+        myTriangles = new int[myXLength * myZLength * 6];
+
+        for (int zLength = 0; zLength < myZLength; zLength++)
+        {
+            for (int xLength = 0; xLength < myXLength; xLength++)
+            {
+                myTriangles[triangles + 0] = vertex + 0;
+                myTriangles[triangles + 1] = vertex + myXLength + 1;
+                myTriangles[triangles + 2] = vertex + 1;
+                myTriangles[triangles + 3] = vertex + 1;
+                myTriangles[triangles + 4] = vertex + myXLength + 1;
+                myTriangles[triangles + 5] = vertex + myXLength + 2;
+
+                vertex++;
+                triangles += 6;
+            }
+
+            vertex++;
+        }
+    }
+
+    private void SetMesh()
+    {
+        myMesh.vertices = myVertices;
+        myMesh.triangles = myTriangles;
+    }
+}
